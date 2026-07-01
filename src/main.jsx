d679916f5c7628203a51124689c9773db4deff26
trilochanpavanreddy737-bit <jpkreddy737c@gmail.com>
2026-07-01 14:18:19 +0530

    Use HashRouter for routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 
@@ -10,12 +10,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-   <App />
-</BrowserRouter>
-      
-      
-      
+    <HashRouter>
+      <App />
+    </HashRouter>
+  
+
+
     
   </StrictMode>
 )
